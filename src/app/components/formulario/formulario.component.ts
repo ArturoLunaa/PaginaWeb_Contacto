aca0d791152597c29cd1ac22bc90e844344ef929
ArturoLunaa <jlg73100@udelasalle.edu.mx>
2021-12-06 18:16:01 -0600

    sección contacto

---
+++ src/app/components/formulario/formulario.component.ts
@@ -0,0 +1,52 @@
+import { BuiltinTypeName } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css'],
+})
+export class FormularioComponent implements OnInit {
+
+ validadorCorreo: any = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+/*Creamos nuestros campos del form*/ 
+  createFormGroup() {
+    return new FormGroup({
+      nombres: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      apellidos: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      telefono: new FormControl('',[Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      correo: new FormControl('',[Validators.required,Validators.pattern(this.validadorCorreo)]),
+      comentarios: new FormControl('',[Validators.required, Validators.maxLength(200)])
+    });
+  }
+
+  formularioContacto: FormGroup;
+  constructor() {
+    this.formularioContacto = this.createFormGroup();
+  }
+
+  ngOnInit() {}
+
+  LimpiarForm(){
+  this.formularioContacto.reset();
+  }
+  Guardar(){
+    if(this.formularioContacto.valid){
+      // alert("Formulario enviado correctamente.");
+      console.log('Formulario válido.');
+      console.log(JSON.stringify(this.formularioContacto.value));
+      alert("Formulario enviado correctamente, sus datos:" + JSON.stringify(this.formularioContacto.value));
+      this.LimpiarForm();
+    }else{
+      alert("Formulario inválido, no se pudo enviar.");
+     console.log("Formulario invalido");
+    }
+  }
+
+ get nombres(){return this.formularioContacto.get('nombres'); }
+ get apellidos(){return this.formularioContacto.get('apellidos');}
+ get telefono(){return this.formularioContacto.get('telefono');}
+ get correo(){return this.formularioContacto.get('correo');}
+ get comentarios(){return this.formularioContacto.get('comentarios');}
+}
